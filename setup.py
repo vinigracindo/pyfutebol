fb5d29ff3def50b7e221a3809f325261237dcde9
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2019-06-10 15:30:05 -0300

    Modifica requirements

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@ from setuptools import setup
 
 
 setup(name='pyfutebol',
-      version='2.0.0',
+      version='2.1.0',
       description='Crawler para resultados de futebol',
       url='https://github.com/vinigracindo/pyfutebol/',
       author='Vinnicyus Gracindo',
       author_email='vini.gracindo@gmail.com',
       license='MIT',
       packages=['pyfutebol'],
+      install_requires=[
+        'beautifulsoup4',
+      ],
       zip_safe=False)
