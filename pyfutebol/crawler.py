ee6a225eab0eb69ee05810f34957dacce9025609
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2019-06-10 18:45:31 -0300

    Adiciona buscar jogo por time

--- pyfutebol/crawler.py
+++ pyfutebol/crawler.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import json
+import re
 from dicttoxml import dicttoxml
 
 
@@ -16,7 +17,6 @@ def jogos_de_hoje(format='dict'):
     results = []
     
     for id, championship in enumerate(championships):
-        matchs_by_championship = []
         matchs = championship.find_all('div', class_='row align-items-center content')
         
         for match in matchs:
@@ -31,6 +31,7 @@ def jogos_de_hoje(format='dict'):
             info = {
                 'match': '{} x {}'.format(team_home, team_visitor),
                 'status': status,
+                'league': titles[id].text,
             }
             
             score = {}
@@ -49,10 +50,7 @@ def jogos_de_hoje(format='dict'):
             
             info.update(score)
             
-            matchs_by_championship.append(info)
-            
-        title = titles[id].text
-        results.append({title: matchs_by_championship})
+            results.append(info)
         
     if (format == 'json'):
         return json.dumps(results)
@@ -63,48 +61,16 @@ def jogos_de_hoje(format='dict'):
 
   
 def jogos_ao_vivo(format='dict'):
-    html = urlopen(url + 'jogos-em-andamento')
-    page = BeautifulSoup(html, 'lxml')
-    titles = page.find_all('h3', class_='match-list_league-name')
-    championships = page.find_all('div', class_='container content')
+    matchs = jogos_de_hoje()
+    results = list(filter(lambda match: re.findall(r'INTERVALO|AO VIVO|MIN', match['status']), matchs))
     
-    results = []
-    
-    for id, championship in enumerate(championships):
-        matchs_by_championship = []
-        matchs = championship.find_all('div', class_='row align-items-center content')
-        
-        for match in matchs:
-            status = match.find('span', class_='status-name').text
-            teams = match.find_all('div', class_='team-name')
-            status = match.find('span', class_='status-name').text
-            scoreboard = match.find_all('span', class_='badge badge-default')
-            
-            team_home = teams[0].text.strip()
-            team_visitor = teams[1].text.strip()
-            
-            info = {
-                'match': '{} x {}'.format(team_home, team_visitor),
-                'status': status,
-                'score': {
-                    team_home: scoreboard[0].text,
-                    team_visitor: scoreboard[1].text
-                },
-                'summary': '{} x {}'.format(scoreboard[0].text, scoreboard[1].text)
-            }
-            
-            score = {}
-            
-            info.update(score)
-            
-            matchs_by_championship.append(info)
-            
-        title = titles[id].text
-        results.append({title: matchs_by_championship})
-        
     if (format == 'json'):
         return json.dumps(results)
     elif (format == 'xml'):
         return dicttoxml(results)
     else:
         return results
+        
+def buscar_jogo_por_time(time):
+    matchs = jogos_de_hoje()
+    return list(filter(lambda match: time.lower() in match['match'].lower(), matchs))
